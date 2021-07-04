55755408df39fc35aef89a3adb83fb9737aad909
Jiuyang Liu <liu@jiuyang.me>
2021-07-04 16:14:22 +0800

    refactor all tests.

--- src/test/scala/chiselTests/PrintableSpec.scala
+++ src/test/scala/chiselTests/PrintableSpec.scala
@@ -4,7 +4,7 @@ package chiselTests
 
 import chisel3._
 import chisel3.stage.ChiselStage
-import chisel3.testers.BasicTester
+import chiselTests.testers.BasicTester
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
